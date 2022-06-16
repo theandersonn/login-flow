26a9dcaf421972fce3acc7add353668f1cad5586
theandersonn <theandersonn@gmail.com>
2022-06-16 17:51:00 -0300

    Estrutura inicial

---
+++ v1/backend/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+const UserRoutes = require('./routes/UserRoutes');
+const app = express();
+
+app.use(express.json());
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use('/users', UserRoutes);
+app.listen(5000);
