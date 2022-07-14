4e65aa196f8b6ba0344b0d7622fd83ce98a89881
theandersonn <theandersonn@gmail.com>
2022-07-13 21:18:55 -0300

    frontend: input no formulario de registrar

--- frontend/src/pages/Register/index.jsx
+++ frontend/src/pages/Register/index.jsx
@@ -1,3 +1,41 @@
+import Input from '../../components/Input';
+
 export const Register = () => {
-  return <h1>Register</h1>;
+  const handleChange = () => {};
+  return (
+    <section>
+      <h1>Registrar</h1>
+      <form>
+        <Input
+          text="Nome"
+          type="text"
+          name="name"
+          placeholder="Digite o seu nome"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="E-mail"
+          type="email"
+          name="email"
+          placeholder="Digite o seu email"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Senha"
+          type="password"
+          name="password"
+          placeholder="Digite o seu nome"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Confirmação de senha"
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirme a sua senha"
+          handleOnChange={handleChange}
+        />
+        <input type="submit" value="Cadastrar" />
+      </form>
+    </section>
+  );
 };
