d640024db8859cf32d7ddf122d6896011b34de82
theandersonn <theandersonn@gmail.com>
2022-07-17 14:31:20 -0300

    frontend: criando formulario de login

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,3 +1,36 @@
+import { useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import Input from '../../components/Input';
+import { Context } from '../../context/UserContext';
+
 export const Login = () => {
-  return <h1>Login</h1>;
+  const handleChange = () => {};
+
+  return (
+    <>
+      <h1>Sign in to app</h1>
+      <form>
+        <Input
+          text="Email"
+          type="email"
+          name="email"
+          placeholder="Enter your email"
+          handleOnChange={handleChange}
+        />
+
+        <Input
+          text="Password"
+          type="password"
+          name="password"
+          placeholder="Enter your password"
+          handleOnChange={handleChange}
+        />
+
+        <input type="submit" value="Sign in" />
+
+        <p>New to App?</p>
+        <Link to="/register">Create your app account</Link>
+      </form>
+    </>
+  );
 };
