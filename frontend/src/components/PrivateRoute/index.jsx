b06910f3f8fbb37bc78146a0b8ee1d0b8deacebd
theandersonn <theandersonn@gmail.com>
2022-07-21 18:11:33 -0300

    frontend: corrige alertas prop-types

--- frontend/src/components/PrivateRoute/index.jsx
+++ frontend/src/components/PrivateRoute/index.jsx
@@ -1,10 +1,15 @@
 import { useContext } from 'react';
+import Proptypes from 'prop-types';
 import { Navigate } from 'react-router-dom';
 import { Context } from '../../context/UserContext';
 
 const PrivateRoute = ({ Component, ...rest }) => {
   const { authenticated } = useContext(Context);
   return authenticated ? <Component {...rest} /> : <Navigate to="/login" />;
+};
+
+PrivateRoute.propTypes = {
+  Component: Proptypes.elementType,
 };
 
 export default PrivateRoute;
--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,15 +1,22 @@
-import { createContext } from 'react';
+import { createContext, useMemo } from 'react';
+import Proptypes from 'prop-types';
 import useAuth from '../hooks/use-auth';
 
 const Context = createContext();
 
 const UserProvider = ({ children }) => {
   const { authenticated, register, logout, login } = useAuth();
-  return (
-    <Context.Provider value={{ authenticated, register, logout, login }}>
-      {children}
-    </Context.Provider>
+  const userProviderValue = useMemo(
+    () => ({ authenticated, register, logout, login }),
+    [authenticated, login, logout, register]
   );
+  return (
+    <Context.Provider value={userProviderValue}>{children}</Context.Provider>
+  );
+};
+
+UserProvider.propTypes = {
+  children: Proptypes.node,
 };
 
 export { Context, UserProvider };
