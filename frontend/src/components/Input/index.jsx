bf1ab9bc249bed8e3bfa0c525f7569b5c84bd3b2
theandersonn <theandersonn@gmail.com>
2022-08-04 11:22:03 -0300

    frontend: add estilos do input

--- frontend/src/components/Input/index.jsx
+++ frontend/src/components/Input/index.jsx
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
 
+import './styles.css';
+
 const Input = ({ type, text, name, placeholder, handleOnChange, value }) => {
   return (
-    <div>
+    <div className="form-control">
       <label htmlFor={name}>{text}:</label>
       <input
         type={type}
